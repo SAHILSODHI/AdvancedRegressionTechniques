a187592051aabb1aa9dad39a88008890d2cd236d
sahil sodhi <sahilsodhi@sahils-MacBook-Pro.local>
2019-03-08 18:52:56 -0500

    Blending 3 models.

--- HousePredictionPrice.py
+++ HousePredictionPrice.py
@@ -13,9 +13,11 @@ from scipy.stats import norm
 from scipy import stats
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_log_error
+from statsmodels.stats.outliers_influence import variance_inflation_factor
 
 train_df = pd.read_csv('train.csv')
 test_df = pd.read_csv('test.csv')
+y = train_df.iloc[:, 80].values
 train_df = train_df.append(test_df, sort = False).reset_index(drop=True)
 test_df_id = test_df.pop('Id')
 
@@ -24,10 +26,8 @@ train_df.info()
 #categ_columns = train_df.select_dtypes(include=['object']).copy().columns.values
 #categ_columns_int = train_df.select_dtypes(include=['int']).copy().columns.values
 
-
 # Nominal Classification
 train_df['MSSubClass']= train_df['MSSubClass'].astype('object')
-
 
 #converting neighborhood to 5 nominal classification based on SalePrice
 neighborhood_map = {
@@ -93,13 +93,22 @@ train_df = train_df[train_df.SalePrice > 0]
 test_df['SalePrice']=test_df['SalePrice'].fillna(value = -1,axis=0)
 test_df = test_df[test_df.SalePrice == -1]
 test_df = test_df.drop(['SalePrice'],1)
-        
+
 # Handling null values for training and test datasets.
 for col in train_df.columns:
     train_df[col]=train_df[col].fillna(value = train_df[col].median(),axis=0)
 
 for col in test_df.columns:
     test_df[col]=test_df[col].fillna(value = test_df[col].median(),axis=0)
+
+vif = pd.DataFrame()
+vif["VIF Factor"] = [variance_inflation_factor(train_df.values, i) for i in range(train_df.shape[1])]
+vif["features"] = train_df.columns
+
+train_df = train_df.drop(['BsmtFinType1_Unf','Electrical_SBrkr','GarageType_Attchd','MasVnrType_None','MasVnrType_None',
+                          'GarageCond','GarageCars','BsmtFinType2_Unf','HouseAge','BsmtFinType2_Rec','HouseStyle'],1)
+test_df = test_df.drop(['BsmtFinType1_Unf','Electrical_SBrkr','GarageType_Attchd','MasVnrType_None','MasVnrType_None',
+                          'GarageCond','GarageCars','BsmtFinType2_Unf','HouseAge','BsmtFinType2_Rec','HouseStyle'],1)
 
 #find correlation of independent variables with the target variable.
 correlation = train_df[train_df.columns[1:]].corr()['SalePrice'][:].sort_values(ascending = True)
@@ -129,7 +138,7 @@ fig = sns.boxplot(x=var, y="SalePrice", data=data)
 fig.axis(ymin=0, ymax=800000);
 
 # HouseAge vs Sale Price
-sns.jointplot(x=train_df['HouseAge'], y=train_df['SalePrice'], kind='reg')
+#sns.jointplot(x=train_df['HouseAge'], y=train_df['SalePrice'], kind='reg')
 
 # YearsSinceRemodelled vs Sale Price
 sns.jointplot(x=train_df['YearsSinceRemodelled'], y=train_df['SalePrice'], kind='reg')
@@ -156,11 +165,6 @@ plt.show()
 
 print("Skewness: %f" % train_df['SalePrice'].skew())
 print("Kurtosis: %f" % train_df['SalePrice'].kurt())
-
-# To resolve skewness, bring the value close to 0. Can further apply box-cox transformation for dependent features.
-train_df["SalePrice"] = np.log1p(train_df["SalePrice"])
-print("Skewness: %f" % train_df['SalePrice'].skew())
-
 train_df.columns.get_loc("SalePrice")
 df1 = train_df.pop('SalePrice')
 train_df['SalePrice'] = df1
@@ -175,23 +179,25 @@ X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 X_test_df = sc.transform(test_df)
 
+
 #feature extraction using PCA
 # number of principle components which accounts for more than 85 percent variance.
 from sklearn.decomposition import PCA
-pca = PCA(n_components = 22)
+pca = PCA(n_components = 30)
 X_train = pca.fit_transform(X_train)
 X_test = pca.transform(X_test)
 X_test_df = pca.transform(X_test_df)
 explained_variance = pca.explained_variance_ratio_
-
 
 #Linear Regression
 from sklearn.linear_model import LinearRegression
 lin_reg = LinearRegression(normalize = False)
 lin_reg.fit(X_train, y_train)
 
+
 lin_pred=lin_reg.predict(X_test)
 RMLSE = np.sqrt(mean_squared_log_error(y_test,lin_pred))
+print(RMLSE)
 
 #Plot to visualize prediction errors.
 #https://scikit-learn.org/0.16/auto_examples/plot_cv_predict.html
@@ -210,23 +216,23 @@ accuracies.std()
 
 #Lasso Regression
 from sklearn.linear_model import Lasso
-lass_reg = Lasso(normalize=True,alpha=0.0001,max_iter=50)
+lass_reg = Lasso(normalize=True,alpha=0.8,max_iter=5)
 lass_reg=lass_reg.fit(X_train,y_train)
 #Predicting test dataset for lasso regression model
 lasso_pred=lass_reg.predict(X_test)
 RMLSE = np.sqrt(mean_squared_log_error(y_test,lasso_pred))
-
+print(RMLSE)
 # Applying Grid Search to find the best model and the best parameters
 from sklearn.model_selection import GridSearchCV
-parameters = [{'alpha': [0.00001,0.0001,0.001,0.01], 'max_iter': [50,100,200]}]
-grid_search = GridSearchCV(estimator = lass_reg,
-                           param_grid = parameters,
-                           scoring = 'explained_variance',
-                           cv = 10,
-                           n_jobs = -1)
-grid_search = grid_search.fit(X_train, y_train)
-best_accuracy = grid_search.best_score_
-best_parameters = grid_search.best_params_
+#parameters = [{'alpha': [0.00001,0.0001,0.001,0.01,0.1,0.8], 'max_iter': [5,50,100,200]}]
+#grid_search = GridSearchCV(estimator = lass_reg,
+#                           param_grid = parameters,
+#                           scoring = 'explained_variance',
+#                           cv = 10,
+#                           n_jobs = -1)
+#grid_search = grid_search.fit(X_train, y_train)
+#best_accuracy = grid_search.best_score_
+#best_parameters = grid_search.best_params_
 
 from sklearn.model_selection import cross_val_score
 accuracies = cross_val_score(estimator = lass_reg, X = X_train, y = y_train, cv = 10)
@@ -249,8 +255,6 @@ ridg_reg = ridg_reg.fit(X_train, y_train)
 ridg_pred = ridg_reg.predict(X_test)
 RMLSE = np.sqrt(mean_squared_log_error(y_test, ridg_pred))
 
-
-
 from sklearn.model_selection import cross_val_score
 accuracies = cross_val_score(estimator = ridg_reg, X = X_train, y = y_train, cv = 10)
 accuracies.mean()
@@ -272,8 +276,6 @@ rforest=rforest.fit(X_train,y_train)
 forest_pred=rforest.predict(X_test)
 RMLSE = np.sqrt(mean_squared_log_error(y_test, forest_pred))
 
-
-
 #Plotting
 fig, ax = plt.subplots()
 ax.scatter(y_test, forest_pred, edgecolors=(0, 0, 0), alpha=1, color='red')
@@ -293,8 +295,6 @@ gboost=clf.fit(X_train,y_train)
 gb_pred=gboost.predict(X_test)
 RMLSE = np.sqrt(mean_squared_log_error(y_test, gb_pred))
 
-
-
 #Plotting
 fig, ax = plt.subplots()
 ax.scatter(y_test, gb_pred, edgecolors=(0, 0, 0), alpha=1, color='red')
@@ -303,27 +303,64 @@ ax.set(xlabel='Measured', ylabel='Predicted')
 plt.title('Gradient Boosting Regressor')
 plt.show()
 
+from sklearn.model_selection import cross_val_score
+accuracies = cross_val_score(estimator = gboost, X = X_train, y = y_train, cv = 10)
+accuracies.mean()
+accuracies.std()
+
+from sklearn.linear_model import Lasso
+lass_reg = Lasso(normalize=True,alpha=0.8,max_iter=5)
+lass_reg=lass_reg.fit(X_train,y_train)
+#Predicting test dataset for lasso regression model
+lasso_pred=lass_reg.predict(X_test)
+RMLSE = np.sqrt(mean_squared_log_error(y_test,lasso_pred))
+print("Gradient Boosting:", RMLSE)
+
+# Applying Grid Search to find the best model and the best parameters
+#from sklearn.model_selection import GridSearchCV
+#parameters = [{'alpha': [0.00001,0.0001,0.001,0.01,0.1,0.8], 'max_iter': [5,50,100,200]}]
+#grid_search = GridSearchCV(estimator = lass_reg,
+#                           param_grid = parameters,
+#                           scoring = 'explained_variance',
+#                           cv = 10,
+#                           n_jobs = -1)
+#grid_search = grid_search.fit(X_train, y_train)
+#best_accuracy = grid_search.best_score_
+#best_parameters = grid_search.best_params_
 
 #XGBoost
 import xgboost as xgb
 regr = xgb.XGBRegressor(
-                        colsample_bytree=0.2,
-                        learning_rate=0.07,
-                        max_depth=4,
-                        min_child_weight=2,
-                        n_estimators=4000,
-                        reg_alpha=0.9,
+                        colsample_bytree=0.8,
+                        learning_rate=0.04,
+                        max_depth=5,
+                        min_child_weight=1,
+                        n_estimators=400,
+                        reg_alpha=1,
                         reg_lambda=0.6,
-                        subsample=0.2,
+                        subsample=0.6,
                         silent=True)
 
-regr.fit(X_train, y_train)
+regr=regr.fit(X_train, y_train)
 xg_pred = regr.predict(X_test)
 RMLSE = np.sqrt(mean_squared_log_error(y_test, xg_pred))
+
+#0.7, 0.05, 4, 1000, 1, 0.6
+from sklearn.model_selection import GridSearchCV
+#parameters = [{'colsample_bytree': [0.7,0.8], 'learning_rate': [0.04,0.05]
+#                ,'min_child_weight':[4,5],'n_estimators':[500,1000,1500], 'reg_alpha':[1,10,50]
+#                , 'subsample':[0.4,0.6]}]
+#grid_search = GridSearchCV(estimator = regr,
+#                           param_grid = parameters,
+#                           scoring = 'explained_variance',
+#                           cv = 10,
+#                           n_jobs = -1)
+#grid_search = grid_search.fit(X_train, y_train)
+#best_accuracy = grid_search.best_score_
+#best_parameters = grid_search.best_params_
+
 #0.011492987568884776
-print ("Root Mean Square Logarithmic Error (Blending Models)=",RMLSE)
-
-
+print ("Root Mean Square Logarithmic Error (XG Boost)=",RMLSE)
 
 #Plotting
 fig, ax = plt.subplots()
@@ -333,7 +370,6 @@ ax.set(xlabel='Measured', ylabel='Predicted')
 plt.title('Extreme Gradient Boosting(XG Boost)')
 plt.show()
 
-
 #AdaBoost
 from sklearn.ensemble import AdaBoostRegressor
 from sklearn.tree import DecisionTreeRegressor
@@ -342,8 +378,7 @@ regr_ada = AdaBoostRegressor(DecisionTreeRegressor(max_depth=15),n_estimators=60
 regr_ada.fit(X_train, y_train)
 ada_pred=regr_ada.predict(X_test)
 RMLSE = np.sqrt(mean_squared_log_error(y_test, ada_pred))
-print ("Root Mean Square Logarithmic Error (Blending Models)=",RMLSE)
-
+print ("Root Mean Square Logarithmic Error (Adaptive Boosting)=",RMLSE)
 
 #Plotting
 fig, ax = plt.subplots()
@@ -357,19 +392,23 @@ y_ada = regr_ada.predict(X_test_df)
 y_xgb = regr.predict(X_test_df)
 y_gboost = gboost.predict(X_test_df)
 y_rforest = rforest.predict(X_test_df)
-y_pred = (y_ada+y_xgb+y_gboost+y_rforest)/4
-
-y_pred2 = np.expm1(y_pred)
-y_pred4 = np.e**(y_pred) - 1
+y_pred = (y_xgb+y_gboost+y_rforest)/3
 
 #Blending Models
-blend_pred=(ada_pred+gb_pred+xg_pred+forest_pred)/4
+blend_pred=(gb_pred+xg_pred+forest_pred)/3
 RMLSE = np.sqrt(mean_squared_log_error(y_test, blend_pred))
 print ("Root Mean Square Logarithmic Error (Blending Models)=",RMLSE)
 
-submission = pd.DataFrame({
-        "Id": test_df_id,
-        "SalePrice": y_pred2
-    })
-    
-submission.to_csv('submission.csv', index=False)
+#Plotting
+fig, ax = plt.subplots()
+ax.scatter(y_test, blend_pred, edgecolors=(0, 0, 0), alpha=1, color='red')
+ax.plot([y_test.min(), y_test.max()], [y_test.min(), y_test.max()], '--', lw=3, color = 'black')
+ax.set(xlabel='Measured', ylabel='Predicted')
+plt.title('Models Blended')
+plt.show()
+
+#submission = pd.DataFrame({
+#        "Id": test_df_id,
+#        "SalePrice": y_pred
+#    })   
+#submission.to_csv('submission.csv', index=False)
